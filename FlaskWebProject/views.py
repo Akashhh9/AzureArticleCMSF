728a65effdcb61e2b1b3ebd2ef1ea9317a4c1146
Akash Choudhary <akash9112002@gmail.com>
2026-03-16 18:49:03 +0530

    Log admin login success message

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -70,8 +70,8 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
 
-        app.logger.info("admin logged in successfully")
         login_user(user, remember=form.remember_me.data)
+        app.logger.info("admin logged in successfully")
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
